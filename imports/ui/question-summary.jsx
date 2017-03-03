beda3b7c0b7543a26a69a5605ac719d77a6d3135
Ignas Strimaitis <strimaitis.ignas@gmail.com>
2017-03-04 00:08:39 +0200

    Show shortest label length in summary too

--- imports/ui/question-summary.jsx
+++ imports/ui/question-summary.jsx
@@ -19,14 +19,20 @@ export default class QuestionSummary extends Component {
     const imagesNum = rows.filter(imageUploaded).length
       + cols.filter(imageUploaded).length;
 
-    // Find longest labels
+    // Find longest and shortest labels
     let longestRowLabel = 0;
     let longestColLabel = 0;
+    let shortestRowLabel = rows[0].label ? rows[0].label.length : 0;
+    let shortestColLabel = cols[0].label ? cols[0].label.length : 0;
     for (let i = 0; i < rowsLength; i++) {
-      longestRowLabel = Math.max(rows[i].label.length, longestRowLabel);
+      const length = rows[i].label.length;
+      longestRowLabel = Math.max(length, longestRowLabel);
+      shortestRowLabel = Math.min(length, shortestRowLabel);
     }
     for (let i = 0; i < colsLength; i++) {
-      longestColLabel = Math.max(cols[i].label.length, longestColLabel);
+      const length = cols[i].label.length;
+      longestColLabel = Math.max(length, longestColLabel);
+      shortestColLabel = Math.min(length, shortestColLabel);
     }
 
     return (
@@ -37,7 +43,9 @@ export default class QuestionSummary extends Component {
           { this.renderSummaryLine("Number of columns", colsLength) }
           { this.renderSummaryLine("Number of images uploaded", imagesNum) }
           { this.renderSummaryLine("Longest row label", longestRowLabel) }
+          { this.renderSummaryLine("Shortest row label", shortestRowLabel) }
           { this.renderSummaryLine("Longest column label", longestColLabel) }
+          { this.renderSummaryLine("Shortest column label", shortestColLabel) }
         </div>
       </div>
     );
